951a84a3aa83d8885c2e6ee3ece4683540dcaecf
Anshuman Pandey <129493396+Anshuman0737@users.noreply.github.com>
2025-09-06 09:14:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+// Typed.js for Hero typing effect
 var typed = new Typed('#typing', {
-  strings: ["AI Enthusiast", "Full-Stack Developer", "Problem Solver"],
+  strings: ["AI Enthusiast", "Full-Stack Developer", "Problem Solver", "Open Source Contributor"],
   typeSpeed: 80,
   backSpeed: 40,
   loop: true
